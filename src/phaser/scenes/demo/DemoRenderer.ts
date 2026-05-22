93c439bb933d4fd75b360724e108468aa270a258
diegodoumecq <licenciado.diego@gmail.com>
2026-05-22 18:22:16 -0300

    simplify demo renderer

--- src/phaser/scenes/demo/DemoRenderer.ts
+++ src/phaser/scenes/demo/DemoRenderer.ts
@@ -4,25 +4,16 @@ import type { AsteroidBodies } from '../../asteroids/bodies';
 import type { AsteroidEntity } from '../../asteroids/types';
 import type { MatterImage, WorldSize } from '../../core/types';
 import type { PlanetEntity } from '../../planets/types';
-import {
-  renderPlayerFuel,
-  renderPlayerShield,
-  renderPlayerThruster,
-  renderPlayerTurret,
-} from '../../player/rendering';
+import { renderPlayerFuel, renderPlayerTurret } from '../../player/rendering';
 import type { ShipState } from '../../player/shipState';
 import type { PlayerState } from '../../player/state';
-import { PLAYER_TURRET_TEXTURE_KEY } from '../../player/textures';
 import { Hud } from '../../ui/Hud';
 import { Minimap } from '../../ui/Minimap';
 
 export class DemoRenderer {
-  private readonly playerTurret: Phaser.GameObjects.Image;
-  private readonly playerShield: Phaser.GameObjects.Graphics;
   private readonly playerFuelBase: Phaser.GameObjects.Graphics;
   private readonly playerFuelFill: Phaser.GameObjects.Graphics;
   private readonly playerFuelMask: Phaser.GameObjects.Graphics;
-  private readonly playerThruster: Phaser.GameObjects.Graphics;
   private readonly collisionMasks: Phaser.GameObjects.Graphics;
   private readonly hud: Hud;
   private readonly minimap: Minimap;
@@ -33,13 +24,10 @@ export class DemoRenderer {
     private readonly asteroidBodies: AsteroidBodies,
     private readonly world: WorldSize,
   ) {
-    this.playerTurret = scene.add.image(player.x, player.y, PLAYER_TURRET_TEXTURE_KEY).setDepth(3);
-    this.playerShield = scene.add.graphics();
     this.playerFuelBase = scene.add.graphics().setDepth(2);
     this.playerFuelFill = scene.add.graphics().setDepth(2);
     this.playerFuelMask = scene.make.graphics({ x: 0, y: 0 }, false);
     this.playerFuelFill.setMask(this.playerFuelMask.createGeometryMask());
-    this.playerThruster = scene.add.graphics().setDepth(0);
     this.collisionMasks = scene.add.graphics().setDepth(20);
     this.hud = new Hud(scene);
     this.minimap = new Minimap(scene);
@@ -52,14 +40,6 @@ export class DemoRenderer {
     planets: PlanetEntity[];
     ship: ShipState;
   }): void {
-    renderPlayerThruster(
-      this.playerThruster,
-      this.player,
-      input.player.lastThrustMove,
-      input.ship.fuel > 0,
-      input.player.thrusting,
-    );
-    renderPlayerTurret(this.player, this.playerTurret, input.player.lastAim, true);
     renderPlayerFuel(
       this.playerFuelBase,
       this.playerFuelFill,
@@ -69,7 +49,6 @@ export class DemoRenderer {
       input.now,
       true,
     );
-    renderPlayerShield(this.playerShield, this.player, false, input.ship.fuel, true);
     this.renderCollisionMasks(input);
     this.minimap.render({
       asteroids: input.asteroids,
